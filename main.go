f50ab3c004439d53996c90720e66e8e1b7bcb9e4
imaemo <tcpln2009@gmail.com>
2019-07-04 17:09:49 +0800

    :sparkles: add log

--- main.go
+++ main.go
@@ -2,16 +2,26 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+
+	"os"
+	"io"
 )
 
 
 func main() {
+	gin.DisableConsoleColor()
+	f, _ := os.Create("gin.log")
+	errorFile, _ := os.Create("error.log")
+
+	gin.DefaultWriter = io.MultiWriter(f)
+	gin.DefaultErrorWriter = io.MultiWriter(errorFile)
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run(":8080") // listen and serve on 0.0.0.0:8080
+	r.Run(":8888") // listen and serve on 0.0.0.0:8080
 	
 }
